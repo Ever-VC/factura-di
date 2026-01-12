b1dbfe9f477eb05b84e6843c94a646297aab5980
Ever-VC <ever.vasquezc27@gmail.com>
2026-01-12 09:30:47 -0600

    🎉 primer commit: Proyecto base

---
+++ src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/SpingbootDifacturaApplication.java
@@ -0,0 +1,13 @@
+package com.evervc.springboot.di.factura.spingbootdifactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootDifacturaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingbootDifacturaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evervc/springboot/di/factura/spingbootdifactura/SpingbootDifacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evervc.springboot.di.factura.spingbootdifactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootDifacturaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
