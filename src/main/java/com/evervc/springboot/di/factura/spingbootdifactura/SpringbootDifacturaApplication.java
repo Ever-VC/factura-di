6e660383a21a0e32054ae8d06cd7a93d030b60f8
Ever-VC <ever.vasquezc27@gmail.com>
2026-01-12 14:36:25 -0600

    :sparkles: feat(SpringbootDiFactura): Renombrando la clase principal

--- src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/SpringbootDifacturaApplication.java
+++ src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/SpringbootDifacturaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpingbootDifacturaApplication {
+public class SpringbootDifacturaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpingbootDifacturaApplication.class, args);
+        SpringApplication.run(SpringbootDifacturaApplication.class, args);
     }
 
 }
