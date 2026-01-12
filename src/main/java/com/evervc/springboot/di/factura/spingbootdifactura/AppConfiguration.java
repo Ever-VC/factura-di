dfe6c5c582a39a5b5c6d219265d07d00a9e4618e
Ever-VC <ever.vasquezc27@gmail.com>
2026-01-12 13:28:06 -0600

    :sparkles: feat(models): Agregando otro método para tener 2 beans y definir uno primario

--- src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/AppConfiguration.java
+++ src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/AppConfiguration.java
@@ -4,6 +4,7 @@ import com.evervc.springboot.di.factura.spingbootdifactura.models.Item;
 import com.evervc.springboot.di.factura.spingbootdifactura.models.Product;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import java.util.Arrays;
@@ -18,6 +19,17 @@ public class AppConfiguration {
         return Arrays.asList(
                 new Item(new Product("Teclado Redragon", 49.95), 2),
                 new Item(new Product("Laptop MSI", 750), 4)
+        );
+    }
+
+    @Bean
+    @Primary
+    List<Item> itemsInvoiceOffice() {
+        return Arrays.asList(
+                new Item(new Product("Monitor ASUS", 187.95), 2),
+                new Item(new Product("Escritorio AnyDesk", 140), 1),
+                new Item(new Product("Silla de Oficina", 124.95), 4),
+                new Item(new Product("Laptop MSI", 950), 1)
         );
     }
 }
