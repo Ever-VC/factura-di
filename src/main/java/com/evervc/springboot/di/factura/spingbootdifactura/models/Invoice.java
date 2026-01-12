ac3bf9d1616c16f49be9cdb2fa69999c98dc4a7f
Ever-VC <ever.vasquezc27@gmail.com>
2026-01-12 13:48:43 -0600

    :sparkles: feat(models/invoice): Ciclo dew viedea de los componentes con PostConstruct y PreDestroy

--- src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/models/Invoice.java
+++ src/main/java/com/evervc/springboot/di/factura/spingbootdifactura/models/Invoice.java
@@ -1,8 +1,13 @@
 package com.evervc.springboot.di.factura.spingbootdifactura.models;
 
+import com.evervc.springboot.di.factura.spingbootdifactura.controllers.InvoiceController;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -22,6 +27,22 @@ public class Invoice {
     private String description;
 
     private final List<Item> items; // Cada item tiene la relación a los productos
+
+    private static final Logger log = LoggerFactory.getLogger(InvoiceController.class);
+
+    @PostConstruct
+    public void init() {
+        // Se pueden modificar los datos (Cosa que si se hace en el constructor falla porque los objetos siguen siendo null)
+        //ya que el contenedor de Spring Boot ya ha inyectado cada elemento (y cuando internamente crea la nueva instancia no lo hace aún)
+        log.warn("OJITO EH: CREANDO EL COMPONENTE DE LA FACTURA");
+        log.info(client.getName());
+        client.setLastname(client.getLastname().concat(" Canales"));
+    }
+
+    @PreDestroy
+    public void destroy() {
+        log.info("Destruyendo el componente invoice");
+    }
 
     public double getTotal() {
         return items.stream().reduce(0.0, (subtotal, item) -> subtotal + item.getAmount(), Double::sum);
